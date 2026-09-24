7e495c15d304d21809dec415bd2cc2dd91767527
Abhijit <abhijitmeher645@gmail.com>
2026-09-24 12:37:50 +0530

    Testapp add

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test {
+
+    public static boolean isEvenNo(Integer n){
+
+        if(n%2==0){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        
+        boolean isEven = Test.isEvenNo(10);
+
+        System.out.println(isEven);
+
+        System.out.println("Hello World... !!");
+        
+    }
+}
+
